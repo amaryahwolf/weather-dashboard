ea01e4fb0d456ce828212a896e03c52c0e8056db
amaryahwolf <115281632+amaryahwolf@users.noreply.github.com>
2022-12-15 21:26:25 -0800

    add 5 day forecast functionality

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,71 @@
-// Base weather URL
-// var weatherUrlBase = api.openweathermap.org/data/2.5/forecast?q={city name}&appid=9c63818d2a58372824ad020aa4224924
-
 // Global variables
 var submitBtn = document.querySelector('#submit-btn');
 var currentWeatherCard = document.querySelector('#current-weather');
+var day1Card = document.querySelector('#day1forecast')
 
-// Function for search bar
+
+// Function to use geocoder API to get lat/lon from city name
 function runWeatherSearch(event) {
     event.preventDefault();
-
-var searchInputVal = document.querySelector('#city-input').value;
-var weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + searchInputVal + '&appid=9c63818d2a58372824ad020aa4224924';
-
-// Fetch data from weather api given the city name entered
-    fetch(weatherUrl)
+    var searchInputVal = document.querySelector('#city-input').value;
+    var geocodeUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchInputVal + '&appid=9c63818d2a58372824ad020aa4224924'
+    fetch(geocodeUrl)
     .then(function (response) {
         return response.json();
     })
-    .then(function (data) {
+    .then (function (data) {
         console.log(data)
-// Print current weather
-// City name
+        // // City name
         var cityName = document.createElement('h3');
-        cityName.textContent = data.city.name;
+        cityName.textContent = data[0].name;
         currentWeatherCard.appendChild(cityName);
-// Date
-// Weather Icon
-// Avg Temperature
-// Wind
-// Humidity  
-    
-// Print 5-day forecast
-// Date
-// Weather Icon
-// High Temp
-// Low Temp
-// Wind
-// Humidity
-    });  
 
+        var latValue = data[0].lat;
+        var lonValue = data[0].lon;
+
+        getWeather(latValue, lonValue);
+    })
+}
+
+// Function to get current day's weather
+
+// Function to add lat/lon to 5-day forecast API call
+function getWeather(latValue, lonValue) {
+    var weatherUrlBase = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latValue + '&lon=' + lonValue + '&units=imperial&appid=9c63818d2a58372824ad020aa4224924'
+    fetch(weatherUrlBase)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+
+            // Date
+            var day1Date = document.createElement('p');
+            day1Date.textContent = data.list[3].dt_txt.slice(0, 11);
+            day1Card.appendChild(day1Date);
+
+            // Weather icon
+            var day1IconCode = data.list[3].weather[0].icon;
+            var day1IconUrl = 'http://openweathermap.org/img/wn/' + day1IconCode + '@2x.png';
+            var day1Icon = document.createElement('img');
+            day1Icon.src = day1IconUrl;
+            day1Card.appendChild(day1Icon);
+
+            // Temperature
+            var day1Temp = document.createElement('p');
+            day1Temp.textContent = data.list[3].main.temp + '°F';
+            day1Card.appendChild(day1Temp);
+
+            // Wind
+            var day1Wind = document.createElement('p');
+            day1Wind.textContent = data.list[3].wind.speed + 'MPH';
+            day1Card.appendChild(day1Wind);
+
+            // Humidity
+            var day1Humidity = document.createElement('p');
+            day1Humidity.textContent = data.list[3].main.humidity + '%';
+            day1Card.appendChild(day1Humidity);
+})
 }
 
 // Add searchAgain function to clear previous search data
